5d55f9926a2cbbcb50a23b57536f75500aca9e4e
hari-haran17 <70980715+hari-haran17@users.noreply.github.com>
2021-11-19 19:20:15 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -9,10 +9,77 @@ class SNAKE:
         self.direction = Vector2(1,0)
         self.new_block = False
 
+        self.body_bl = pygame.image.load('Images/body_bl.png').convert_alpha()
+        self.body_br = pygame.image.load('Images/body_br.png').convert_alpha()
+        self.body_horizontal = pygame.image.load('Images/body_horizontal.png').convert_alpha()
+        self.body_tl = pygame.image.load('Images/body_tl.png').convert_alpha()
+        self.body_tr = pygame.image.load('Images/body_tr.png').convert_alpha()
+        self.body_vertical = pygame.image.load('Images/body_vertical.png').convert_alpha()
+        self.head_down = pygame.image.load('Images/head_down.png').convert_alpha()
+        self.head_left = pygame.image.load('Images/head_left.png').convert_alpha()
+        self.head_right = pygame.image.load('Images/head_right.png').convert_alpha()
+        self.head_up = pygame.image.load('Images/head_up.png').convert_alpha()
+        self.tail_down = pygame.image.load('Images/tail_down.png').convert_alpha()
+        self.tail_left = pygame.image.load('Images/tail_left.png').convert_alpha()
+        self.tail_right = pygame.image.load('Images/tail_right.png').convert_alpha()
+        self.tail_up = pygame.image.load('Images/tail_up.png').convert_alpha()
+
+        self.crunch_sound = pygame.mixer.Sound('Sound/Sound_crunch.wav')
+        #self.bg_sound = pygame.mixer.Sound('Sound/African Safari Loop.wav')
+
+
+
     def draw_snake(self):
-        for block in self.body:
+        self.update_head_graphics()
+        self.update_tail_graphics()
+
+        for index,block in enumerate(self.body):
             block_rect = pygame.Rect(block.x*block_size, block.y*block_size, block_size, block_size)
-            pygame.draw.rect(screen, (93, 63, 211), block_rect)
+            #pygame.draw.rect(screen, (93, 63, 211), block_rect)
+            if index == 0:
+                screen.blit(self.head, block_rect)
+            elif index == (len(self.body) - 1):
+                screen.blit(self.tail, block_rect)
+            else:
+                previous_block = self.body[index+1] - block
+                next_block = self.body[index-1] - block
+                if previous_block.x == next_block.x:
+                    screen.blit(self.body_vertical, block_rect)
+                elif previous_block.y == next_block.y:
+                    screen.blit(self.body_horizontal, block_rect)
+                else:
+                    if previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1:
+                        screen.blit(self.body_tl, block_rect)
+                    elif previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1:
+                        screen.blit(self.body_bl, block_rect)
+                    elif previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1:
+                        screen.blit(self.body_tr, block_rect)
+                    elif previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1:
+                        screen.blit(self.body_br, block_rect)
+
+
+    def update_head_graphics(self):
+        head_relation = self.body[1] - self.body[0]
+        if head_relation == Vector2(1,0):
+            self.head = self.head_left
+        elif head_relation == Vector2(-1,0):
+            self.head = self.head_right
+        elif head_relation == Vector2(0,1):
+            self.head = self.head_up
+        elif head_relation == Vector2(0,-1):
+            self.head = self.head_down
+
+    def update_tail_graphics(self):
+        tail_relation = self.body[-2] - self.body[-1]
+        if tail_relation == Vector2(1, 0):
+            self.tail = self.tail_left
+        elif tail_relation == Vector2(-1, 0):
+            self.tail = self.tail_right
+        elif tail_relation == Vector2(0, 1):
+            self.tail = self.tail_up
+        elif tail_relation == Vector2(0, -1):
+            self.tail = self.tail_down
+
 
     def move_snake(self):
         if self.new_block == True:
@@ -27,6 +94,12 @@ class SNAKE:
 
     def add_block(self):
         self.new_block = True
+
+    def play_crunch_sound(self):
+        self.crunch_sound.play()
+
+    #def play_bg_sound(self):
+        #self.bg_sound.play()
 
 
 
@@ -50,6 +123,7 @@ class MAIN:
         self.fruit = FRUIT()
 
     def update(self):
+        #self.snake.play_bg_sound()
         self.snake.move_snake()
         self.check_collision()
         self.check_fail()
@@ -57,11 +131,13 @@ class MAIN:
     def draw_elements(self):
         self.fruit.draw_fruit()
         self.snake.draw_snake()
+        self.draw_score()
 
     def check_collision(self):
         if self.fruit.pos == self.snake.body[0]:
             self.fruit.randomize()
             self.snake.add_block()
+            self.snake.play_crunch_sound()
 
     def check_fail(self):
         if not 0 <= self.snake.body[0].x < block_no or not 0 <= self.snake.body[0].y < block_no:
@@ -74,6 +150,22 @@ class MAIN:
         pygame.quit()
         sys.exit()
 
+    def draw_score(self):
+        score_text = str(len(self.snake.body) -3)
+        score_surface = game_font.render(score_text, True, (56,74,12))
+        score_x = int(block_size*block_no - 60)
+        score_y = int(block_no*block_size - 40)
+        score_rect = score_surface.get_rect(center = (score_x, score_y))
+
+        apple_rect = apple.get_rect(midright = (score_rect.left, score_rect.centery))
+        bg_rect = pygame.Rect(apple_rect.left, apple_rect.top, apple_rect.width+score_rect.width+7, apple_rect.height)
+        pygame.draw.rect(screen, (167,209,61), bg_rect)
+        screen.blit(score_surface, score_rect)
+        screen.blit(apple,apple_rect)
+        pygame.draw.rect(screen, (56,74,12), bg_rect,2)
+
+
+pygame.mixer.pre_init(44100,-16,2,512)
 pygame.init()
 
 block_size = 40
@@ -81,7 +173,8 @@ block_no = 20
 
 screen = pygame.display.set_mode((block_size*block_no,block_size*block_no))
 clock = pygame.time.Clock()
-apple = pygame.image.load('Images/apple.png').convert_alpha()
+apple = pygame.image.load('Images/apple3.png').convert_alpha()
+game_font = pygame.font.Font('Font/Cotton Butter.ttf', 25)
 
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, 150)
